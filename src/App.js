ba4a3a3fdfee6c577ff8989b7a9bd941a3cb3df1
SuchithG <suchithgowda1995@gmail.com>
2023-02-15 21:45:42 +0530

    feat: :sparkles: Passing Data via props

--- src/App.js
+++ src/App.js
@@ -2,10 +2,55 @@ import "./App.css";
 import ExpenseItem from "./components/ExpenseItem";
 
 function App() {
+  const expenses = [
+    {
+      id: "e1",
+      title: "Toilet Paper",
+      amount: 94.12,
+      date: new Date(2020, 7, 14),
+    },
+    {
+      id: "e2",
+      title: "Car Insurance",
+      amount: 294.12,
+      date: new Date(2021, 8, 1),
+    },
+    {
+      id: "e3",
+      title: "New Tv",
+      amount: 82.04,
+      date: new Date(2022, 6, 12),
+    },
+    {
+      id: "e4",
+      title: "New Desk (Wooden)",
+      amount: 450,
+      date: new Date(2021, 5, 10),
+    },
+  ];
   return (
     <div>
       <h2>Let's get started !</h2>
-      <ExpenseItem></ExpenseItem>
+      <ExpenseItem
+        title={expenses[0].title}
+        amount={expenses[0].amount}
+        date={expenses[0].date}
+      ></ExpenseItem>
+      <ExpenseItem
+        title={expenses[1].title}
+        amount={expenses[1].amount}
+        date={expenses[1].date}
+      ></ExpenseItem>
+      <ExpenseItem
+        title={expenses[2].title}
+        amount={expenses[2].amount}
+        date={expenses[2].date}
+      ></ExpenseItem>
+      <ExpenseItem
+        title={expenses[3].title}
+        amount={expenses[3].amount}
+        date={expenses[3].date}
+      ></ExpenseItem>
     </div>
   );
 }
